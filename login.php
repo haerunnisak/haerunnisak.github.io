f2cf29779cb669b3f8434b64d61461452b1380bb
haerunnisak <150058915+haerunnisak@users.noreply.github.com>
2023-12-05 19:12:56 +0700

    login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ WHERE email = '$email', AND password = $password";
 $result = mysqli_query($conn, $query_sql);
 
 if (mysqli_num_rows($result) > 0){
-    header("Location: beranda.html");
+    header("Location: index.html");
 } else {
     echo "<center> <h1>Email atau Password Anda Salah. Silahkan Coba Login Kembali</h1>
     <button><strong> <a href='login.html'>Login</a></strong></button></center>";
